ba82fa3846c45dd289a2b17312a94816493db1d9
Alex Chen <alex@spoonrocket.com>
2017-04-21 01:13:03 -0700

    scaffold for dogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :dogs
+
   get "users", to: "users#index"
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
   post "users", to: "users#create"
+
+  root :to => "users#index"
+
 end
